25dbdd9ff4bc7a605198421fbd43739a22df1c3f
Kishore <89995228+AASFCYBERKING@users.noreply.github.com>
2021-12-16 19:04:29 +0530

    Update broadcast.py

--- bot/plugins/broadcast.py
+++ bot/plugins/broadcast.py
@@ -13,7 +13,7 @@ from bot.sql.users_sql import get_chats
 )
 async def num_start_message(client: Bot, message: Message):
     if not message.reply_to_message:
-        return await message.reply_text("Reply to Message..", quote=True)
+        return await message.reply_text("`Reply to Message..`", quote=True)
     reply = message.reply_to_message
     All = get_chats()
     TTL = len(All)
@@ -25,7 +25,7 @@ async def num_start_message(client: Bot, message: Message):
         except Exception as e:
             print(e, chat)
     MSG = "**BroadCast Completed !**\n"
-    MSG += f"Succeed : {SUCCESS} Chats!"
+    MSG += f"**Succeed :** {SUCCESS} **Chats!**"
     if TTL != SUCCESS:
         MSG += f"\nFailed : {All-SUCCESS} Chats."
     await message.reply_text(MSG, quote=True)
