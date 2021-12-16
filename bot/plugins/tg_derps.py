5902316631e79eda042a7b2cd1d389f536fa5591
Kishore <89995228+AASFCYBERKING@users.noreply.github.com>
2021-12-16 18:18:58 +0530

    Update tg_derps.py

--- bot/plugins/tg_derps.py
+++ bot/plugins/tg_derps.py
@@ -1,27 +1,10 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) Shrimadhav U K
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-
 from pyrogram.types import (
     CallbackQuery,
     Message
 )
 from typing import List
 from bot import (
-    AUTH_CHANNEL,
+    OWNER_ID,
     DELETED_MESSAGES_NOTIFICATION_TEXT,
     DERP_USER_S_TEXT
 )
@@ -47,7 +30,7 @@ async def on_del_mesgs(client: Bot, messages: List[Message]):
             ym = get_chek_dmid(message.message_id)
             if ym:
                 await client.send_message(
-                    chat_id=AUTH_CHANNEL,
+                    chat_id=OWNER_ID,
                     text=DELETED_MESSAGES_NOTIFICATION_TEXT,
                     reply_to_message_id=ym.message_id
                 )
