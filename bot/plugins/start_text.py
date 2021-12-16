aaa1a9ae2258f7322c9672661224bb6e68bed6d7
Kishore <89995228+AASFCYBERKING@users.noreply.github.com>
2021-12-16 20:58:22 +0530

    Update start_text.py

--- bot/plugins/start_text.py
+++ bot/plugins/start_text.py
@@ -15,10 +15,10 @@ from bot.hf.flifi import uszkhvis_chats_ahndler
 
 # wants to add your own text read this https://core.telegram.org/bots/api#html-style 
 START_TEXT = """
-───「 <a href="https://telegra.ph/file/de0013d013b55c4676a36.jpg">AASFCYBERKING</a> 」───
-<b>Hey Master! ,
-I Am Working Properly With Awesome Speed
-➖➖➖➖➖➖➖➖➖➖➖➖➖
+──「 <a href="https://telegra.ph/file/de0013d013b55c4676a36.jpg">AASFCYBERKING</a> 」──
+<b>Hey Master! ,</b>
+<b>I Am Working Properly With Awesome Speed</b>
+<b>➖➖➖➖➖➖➖➖➖➖➖➖➖</b>
 <code>My Repo Is Public</code> <a href="https://github.com/AASFCYBERKING/NoPmBot">Here</a>
 """
 
